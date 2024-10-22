424bea050ea2bdb0b3681a1ba9e64c5202ca909f
joaopedromm <138119359+morais20122008@users.noreply.github.com>
2024-10-22 09:05:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Filosofia',
+    'Quando começou a filosofia na Grécia?',
+    'Surgiu no séc. VI a.C'
+)
+criaCartao(
+    'História',
+    'quando a humanidade passou pela revolução agrícola?',
+    'a mais ou menos 12 mil anos atrás. P.S: não foi no mesmo período em todas as partes do globo.'
+)
+criaCartao(
+    'Geografia',
+    'Em qual continente está localizada a Etiópia?',
+    'Está localizada no continente africano.'
+)
+criaCartao(
+    'matemática',
+    '34 + 20x2?',
+    '74.'
+)
